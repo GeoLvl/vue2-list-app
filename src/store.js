cbdd831dd4c33e77b4a3cb69858162c8a665b152
NyanBot <laville.geoffrey@gmail.com>
2023-03-09 23:00:34 +0100

    add mutations & actions + form create event

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: { id: 'abc123', name: 'bob' },
+    user: { id: 'abc123', name: 'Adam Jahr' },
     categories: [
       'sustainability',
       'nature',
@@ -14,19 +15,24 @@ export default new Vuex.Store({
       'education',
       'food',
       'community'
-    ]
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   getters: {
-    catLength: state => {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: state => {
-      return state.todos.filter(todo => !todo.done).length
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
