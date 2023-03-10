9aecca6b5df28164f962cc97f909871fe7cf87b6
NyanBot <laville.geoffrey@gmail.com>
2023-03-10 23:44:01 +0100

    create store modules

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-        <EventCard v-for="event in events" :key="event.id" :event="event"/>
-        <template v-if="page != 1">
-          <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">Prev Page</router-link> | 
-        </template>
-        <router-link :to="{ name: 'event-list', query: { page: page + 1 } }">Next Page</router-link>
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">
+      Prev Page</router-link> 
+      <template v-if="hasNextPage"> | </template>
+    </template>
+    <router-link v-if="hasNextPage" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">
+      Next Page</router-link>
   </div>
 </template>
 
@@ -17,17 +21,20 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents', {
-      perPage: 3, // <-- How many items to display per page
-      page: this.page // <-- What page we're on
+    this.perPage = 3
+    this.$store.dispatch('event/fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
     })
   },
   computed: {
     page() {
-      // What page we're currently on
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['event', 'user'])
   }
 }
 </script>
